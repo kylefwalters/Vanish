b5d5ffd80da524a6efda08559827bb6c3a0b9b85
kylefwalters <kylefwalters@gmail.com>
2020-11-13 00:07:35 -0500

    Results screen added, remaining items are counted in UI

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -9,7 +9,10 @@ public class LevelManager : MonoBehaviour
 {
     public List<GameObject> objectives = new List<GameObject>();
     private List<GameObject> _objectives = new List<GameObject>();
+    [HideInInspector]
+    public int itemsRemaining;
 
+    private GameObject gameStateManager;
     public GameObject levelData;
     private float timer =30 ;
     [SerializeField, Tooltip("In-game display of timer")]
@@ -18,7 +21,11 @@ public class LevelManager : MonoBehaviour
     public bool isPaused;
     public TimeBar timeBar;
 
+    bool levelLoading = false;
+    public GameObject winMenu;
     int currentScene;
+    public AsyncOperation asyncOpertaion;
+    static int totalAttempts;
 
     void Awake()
     {
@@ -30,11 +37,15 @@ public class LevelManager : MonoBehaviour
             objectives.Add(objectivesArray[i]);
         }
         _objectives = objectives;
+        itemsRemaining = (objectives.Count/2);
 
-        if(levelData!=null)
+        if (gameStateManager == null)
+            gameStateManager = GameObject.Find("Game State Manager");
+        if (levelData==null)
             levelData = GameObject.Find("Level Data");
         timer = levelData.GetComponent<LevelData>().levelTime;
         timeBar.SetMaxTime(timer);
+        GameObject.Find("TotalItem").GetComponent<TextMeshProUGUI>().text = "\n" + "Remaining Items: " + itemsRemaining;
         currentScene = SceneManager.GetActiveScene().buildIndex; //Grabs current scene's index
     }
 
@@ -51,10 +62,20 @@ public class LevelManager : MonoBehaviour
 
             timeBar.SetTime(timer);
         }
+        
+        //For when level is loading
+        if(asyncOpertaion != null)
+        {
+            if (asyncOpertaion.progress >= 0.89)
+            {
+                winMenu.transform.GetChild(6).gameObject.SetActive(true);
+            }
+        }
     }
 
     public void CheckForCompletion() //Checks if Level is complete
     {
+        GameObject.Find("TotalItem").GetComponent<TextMeshProUGUI>().text = "\n" + "Remaining Items: " + itemsRemaining;
         foreach(GameObject objective in objectives)
         {
             if (objective==null || objective.activeInHierarchy == true)
@@ -71,15 +92,45 @@ public class LevelManager : MonoBehaviour
 
     public void onLevelFail()
     {
+        if (levelLoading)
+            return;
+
+        totalAttempts++;
+
         //Play Fail Animation
 
-        LoadLevel(currentScene);
+        SceneManager.LoadScene(currentScene);
+
+        levelLoading = true;
     }
 
     public void LoadLevel(int sceneIndex)
     {
+        if (levelLoading)
+            return;
+
+        totalAttempts++;
+        isPaused = true;
+        gameStateManager.SetActive(false);
+
         //Play Scene Transition
-        print("Loading Level " + SceneManager.GetSceneByBuildIndex(sceneIndex).name);
-        SceneManager.LoadSceneAsync(sceneIndex);
+
+        print("Loading " + SceneManager.GetSceneByBuildIndex(sceneIndex).name);
+        asyncOpertaion = SceneManager.LoadSceneAsync(sceneIndex);
+        asyncOpertaion.allowSceneActivation = false;
+
+        GameObject.Find("Player").GetComponent<topdown_Player>().enabled = false;
+        winMenu.SetActive(true);
+        winMenu.transform.GetChild(3).gameObject.GetComponent<TextMeshProUGUI>().text = string.Format("{0:#.00}s", timer);
+        winMenu.transform.GetChild(5).gameObject.GetComponent<TextMeshProUGUI>().text = totalAttempts.ToString();
+        winMenu.transform.GetChild(6).gameObject.SetActive(false);
+        
+        levelLoading = true;
+    }
+
+    public void FinishLoad() //For use by Continue Button
+    {
+        totalAttempts = 0;
+        asyncOpertaion.allowSceneActivation = true;
     }
 }
