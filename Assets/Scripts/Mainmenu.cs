2ee996378138b50ce7632f1929b4d71d3267c1bc
Kamrin21 <dujiaming0731@gmail.com>
2020-12-06 00:55:21 -0500

    Tutorial, Enemeis. Hard mode.

--- Assets/Scripts/Mainmenu.cs
+++ Assets/Scripts/Mainmenu.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Mainmenu : MonoBehaviour
 {
-    public GameObject level;
+    public GameObject levelE;
+    public GameObject levelH;
     public GameObject main;
     public GameObject credit;
+    public GameObject tutorial;
+    public GameObject diff;
     // Start is called before the first frame update
     void Start()
     {
         main.SetActive(true);
         credit.SetActive(false);
-        level.SetActive(false);
+        levelE.SetActive(false); 
+        levelH.SetActive(false);
+        diff.SetActive(false);
+        tutorial.SetActive(false);
     }
 
     // Update is called once per frame
@@ -33,29 +39,72 @@ public class Mainmenu : MonoBehaviour
     {
         SceneManager.LoadScene("Level1");
     }
+    public void level1H()
+    {
+        SceneManager.LoadScene("Level1H");
+    }
     public void level2N()
     {
         SceneManager.LoadScene("Level2");
+    }
+    public void level2H()
+    {
+        SceneManager.LoadScene("Level2H");
     }
     public void level3N()
     {
         SceneManager.LoadScene("Level3");
     }
+    public void level3H()
+    {
+        SceneManager.LoadScene("Level3H");
+    }
     public void level4N()
     {
         SceneManager.LoadScene("Level4");
+    }
+    public void level4H()
+    {
+        SceneManager.LoadScene("Level4H");
     }
     public void level5N()
     {
         SceneManager.LoadScene("Level5");
     }
+    public void level5H()
+    {
+        SceneManager.LoadScene("Level5H");
+    }
+    public void OnEasyClick()
+    {
+
+
+        diff.SetActive(false);
+        levelE.SetActive(true);
+
+    }
+    public void OnHardClick()
+    {
+
+
+        diff.SetActive(false);
+        levelH.SetActive(true);
+
+    }
+
+
     public void OnlevelClick()
     {
         
         
         main.SetActive(false);
-        level.SetActive(true);
+        diff.SetActive(true);
         
+    }
+    public void OnBackClickDF()
+    {
+        diff.SetActive(false);
+        main.SetActive(true);
     }
     public void OncreditClick()
     {
@@ -66,14 +115,33 @@ public class Mainmenu : MonoBehaviour
 
     }
 
-    public void OnBackClickLV()
+    public void OntutorClick()
     {
-        level.SetActive(false);
-        main.SetActive(true);
+
+
+        main.SetActive(false);
+        tutorial.SetActive(true);
+
+    }
+
+    public void OnBackClickLVE()
+    {
+        levelE.SetActive(false);
+        diff.SetActive(true);
+    }
+    public void OnBackClickLVH()
+    {
+        levelH.SetActive(false);
+        diff.SetActive(true);
     }
     public void OnBackClickCD()
     {
         credit.SetActive(false);
+        main.SetActive(true);
+    }
+    public void OnBackClickTT()
+    {
+        tutorial.SetActive(false);
         main.SetActive(true);
     }
 }
